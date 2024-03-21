627b9087b3b5bdf2537a6f0f160f58a4e4ccc819
Evgeniy Pimenov <contact@evgeniypimenov.com>
2024-03-21 14:08:06 +0100

    working producer

--- projects/basic/index.js
+++ projects/basic/index.js
@@ -3,7 +3,8 @@ const {Kafka, logLevel} = require('kafkajs')
 
 const kafka = new Kafka({
   logLevel: logLevel.INFO,
-  clientId: 'upstash-playground-consumer',
+  // clientId: 'upstash-playground-consumer',
+  clientId: 'upstash-playground-producer',
   brokers: ['upright-boxer-11932-eu2-kafka.upstash.io:9092'],
   ssl: {
     rejectUnauthorized: true
@@ -15,19 +16,64 @@ const kafka = new Kafka({
   }
 })
 
-const consumer = kafka.consumer({groupId: 'consumer'})
+// init consumer
+const consumer = kafka.consumer({groupId: 'node_producer'})
+// init producer
+const producer = kafka.producer({allowAutoTopicCreation: false})
 
-const run = async () => {
+const write = async () => {
+  await producer.connect()
+  await producer.send({
+    topic: 'first_topic',
+    messages: [
+        {value: (Math.random() * 10000).toFixed(1).toString()},
+    ]
+  })
+  // await producer.disconnect()
+}
+
+const read = async () => {
   await consumer.connect()
-  await consumer.subscribe({topic: 'third_topic', fromBeginning: true})
+  // why does the playground ignore the fromBeginning flag?
+  await consumer.subscribe({topic: 'first_topic', fromBeginning: true})
   await consumer.run({
-    eachMessage: (value) => {
-      console.log(value)
+    eachMessage: ({topic, partition, message}) => {
+      const prefix = `${topic}[${partition} | ${message.offset}] / ${message.timestamp}`
+      // deserialized message is at message.value
+      console.log(`- ${prefix} ${message.key}#${message.value}`)
     }
   })
 }
 
-run().catch(e => console.error(e))
+write().catch(e => console.error(e))
+// read().catch(e => console.error(e))
+
+// Code to gracefully disconnect from the Broker in the event of errors or SIGTERM and the like
+const errorTypes = ['unhandledRejection', 'uncaughtException']
+const signalTraps = ['SIGTERM', 'SIGINT', 'SIGUSR2']
+
+errorTypes.forEach(type => {
+  process.on(type, async e => {
+    try {
+      console.log(`process.on ${type}`)
+      console.error(e)
+      await consumer.disconnect()
+      process.exit(0)
+    } catch (_) {
+      process.exit(1)
+    }
+  })
+})
+
+signalTraps.forEach(type => {
+  process.once(type, async () => {
+    try {
+      await consumer.disconnect()
+    } finally {
+      process.kill(process.pid, type)
+    }
+  })
+})
 
 // http.createServer(function (req, res) {
 //   res.writeHead(200, {'Content-Type': 'text/html'});
