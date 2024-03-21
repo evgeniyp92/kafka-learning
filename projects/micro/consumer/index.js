5bad7f36faf9aa7d16f2147c6d48d6bca83c55c4
Evgeniy Pimenov <contact@evgeniypimenov.com>
2024-03-21 15:21:35 +0100

    add documentation

--- projects/micro/consumer/index.js
+++ projects/micro/consumer/index.js
@@ -2,6 +2,19 @@ import { Kafka, logLevel } from 'kafkajs';
 
 console.log('consumer')
 
+/**
+ * Kafka Connection configuration.
+ *
+ * @constructor
+ * @param {Object} config - Configuration options for the Kafka connection.
+ * @param {string[]} config.brokers - An array of broker addresses to connect to.
+ * @param {boolean} [config.ssl=false] - Enable SSL connection.
+ * @param {Object} [config.sasl] - SASL (Simple Authentication and Security Layer) configuration.
+ * @param {string} [config.sasl.mechanism] - The SASL mechanism to use.
+ * @param {string} [config.sasl.username] - The SASL username for authentication.
+ * @param {string} [config.sasl.password] - The SASL password for authentication.
+ * @param {string} [config.logLevel='error'] - The log level for logging Kafka client events and messages.
+ */
 const kafka = new Kafka({
     brokers: ['upright-boxer-11932-eu2-kafka.upstash.io:9092'],
     ssl: true,
@@ -13,8 +26,32 @@ const kafka = new Kafka({
     logLevel: logLevel.ERROR,
 });
 
+/**
+ * Creates a Kafka consumer with the given configuration.
+ *
+ * @param {Object} configuration - The configuration for the consumer.
+ * @param {string} configuration.groupId - The unique identifier for the consumer group.
+ *
+ * @returns {Object} - The Kafka consumer object.
+ */
 const consumer = kafka.consumer({ groupId: 'node_consumer' });
 
+/**
+ * Runs the consumer and starts listening for messages on the specified topic.
+ *
+ * @async
+ * @function run
+ * @returns {Promise<void>}
+ *
+ * @description
+ * The `run` function connects to the consumer, subscribes to the specified topic, and
+ * starts listening for messages. When a new message is received, the `eachMessage` callback
+ * function is invoked with the message details containing the topic, partition, and message data.
+ * The function logs the partition, offset, and value of each message to the console.
+ *
+ * @example
+ * await run();
+ */
 const run = async () => {
     await consumer.connect();
     await consumer.subscribe({ topic: 'test', fromBeginning: true });
--- projects/micro/producer/index.js
+++ projects/micro/producer/index.js
@@ -2,6 +2,18 @@ import { Kafka, logLevel } from 'kafkajs';
 
 console.log('producer')
 
+/**
+ * Kafka configuration object.
+ *
+ * @typedef {Object} KafkaConfig
+ * @property {string[]} brokers - An array of Kafka broker addresses.
+ * @property {boolean} ssl - A boolean indicating whether to use SSL encryption.
+ * @property {object} sasl - SASL authentication configuration.
+ * @property {string} sasl.mechanism - The SASL authentication mechanism to use.
+ * @property {string} sasl.username - The username for SASL authentication.
+ * @property {string} sasl.password - The password for SASL authentication.
+ * @property {string} logLevel - The log level for Kafka client.
+ */
 const kafka = new Kafka({
     brokers: ['upright-boxer-11932-eu2-kafka.upstash.io:9092'],
     ssl: true,
@@ -13,8 +25,23 @@ const kafka = new Kafka({
     logLevel: logLevel.ERROR,
 });
 
+/**
+ * Instantiates a new instance of Kafka producer.
+ *
+ * @class
+ * @classdesc The Producer class is responsible for producing and sending messages to a Kafka topic.
+ *
+ * @returns {KafkaProducer} A new instance of KafkaProducer.
+ */
 const producer = kafka.producer();
 
+/**
+ * A function that sends a message to a Kafka topic using a producer.
+ *
+ * @async
+ * @function run
+ * @returns {Promise<void>} - A promise that resolves when the message is sent successfully and the producer is disconnected.
+ */
 const run = async () => {
     await producer.connect();
 
